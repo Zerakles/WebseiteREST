07b7e3e204f6fef0b160c90d2866efde19c3da0d
Marvin Babig <marvin.babig@mitemma.de>
2023-12-19 14:50:09 +0100

    Added request in tempsensor.py

--- python/Pi/tempsensor.py
+++ python/Pi/tempsensor.py
@@ -1,6 +1,11 @@
 import os
 import time
+from datetime import datetime
+
 import requests
+from Helpers.pi_requests_class import PiRequests
+
+
 
 # Adresse des DS18B20-Sensors im Dateisystem
 sensor_file = '/sys/bus/w1/devices/28-00000cb47b8d/w1_slave'
@@ -10,6 +15,10 @@ api_endpoint = 'DEINE_API_ENDPOINT_URL_HIER_EINFÜGEN'
 
 # Pfad zur TXT-Datei für die Temperatur
 txt_file_path = 'temperatur.txt'
+
+# Userdata
+user_name = 'DEIN_USERNAME_HIER_EINFÜGEN'
+user_password = 'DEIN_PASSWORD_HIER_EINFÜGEN'
 
 def read_temperature():
     try:
@@ -29,14 +38,19 @@ def read_temperature():
     except Exception as e:
         print(f"Fehler beim Lesen der Temperatur: {e}")
         return None
-"""
+
 def send_to_api(temperature):
+    temp_c = temperature
+    temp_f = float(temperature) * 9.0 / 5.0 + 32.0
     try:
         # Daten an das REST-API senden
-        payload = {'temperature': temperature}
-        response = requests.post(api_endpoint, json=payload)
-        response.raise_for_status()
-        print(f"Erfolgreich an API gesendet. Antwort: {response.text}")
+        user_data = PiRequests(f'http://${api_endpoint}:8000/api/v1/users', {'username': user_name, 'password': user_password}, 'get_users')
+        if user_data == 'User not found':
+            PiRequests(f'http://${api_endpoint}:8000/api/v1/users', {'name': user_name, 'password': user_password, 'token': 'admin'}, 'insert_user')
+            user_data = PiRequests(f'http://${api_endpoint}:8000/api/v1/users', {'username': user_name, 'password': user_password}, 'get_users')
+        timestamp = datetime.now().strftime("%Y-%m-%dT%HH:%M:%S")
+        response = PiRequests(f'http://${api_endpoint}:8000/api/v1/temps', {'time': timestamp, 'temp_c': temp_c, 'temp_f': temp_f, 'HID': user_data}, 'insert_temp')
+        print(f"Erfolgreich an API gesendet. Antwort: {response}")
 
     except requests.exceptions.RequestException as api_err:
         print(f"Fehler beim Senden an API: {api_err}")
@@ -50,15 +64,15 @@ def update_txt_file(temperature):
 
     except Exception as txt_err:
         print(f"Fehler beim Aktualisieren der TXT-Datei: {txt_err}")
-"""
+
 # Hauptprogrammschleife
 while True:
     temperature = read_temperature()
-"""""
+
     if temperature is not None:
         send_to_api(temperature)
         update_txt_file(temperature)
 
     # Wartezeit zwischen den Messungen
     time.sleep(1)
-"""
+
