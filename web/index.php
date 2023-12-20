ae7da5a306fc226d6b3e5545a670ce85f51e1191
Zerakles <jonas@grewe.eu>
2023-12-20 11:17:59 +0100

    fixes

--- web/index.php
+++ web/index.php
@@ -13,6 +13,9 @@
 <html lang="DE">
 <head>
     <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/grafik.css">
+    <link rel="stylesheet" href="./css/Table.css">
     <script src="./js/nav.js"></script>
     <title>REST Statistiken</title>
     <?php
