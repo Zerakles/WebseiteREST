8520b7d82ab20d705337d286d5c52fe5172ff083
Luis <luisbittner1.0@gmail.com>
2023-12-20 12:13:47 +0100

    Tabel.js update

--- web/js/Table.js
+++ web/js/Table.js
@@ -1,4 +1,4 @@
-
+/*
 document.addEventListener('DOMContentLoaded', function () {
     //const apiUrl = 'API_URL'; // Ersetze dies durch die tatsächliche API-URL
     const apiUrl = '';
@@ -11,8 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function updateTable(data) {
-        const tableBody = document.getElementById('table-body');
-        tableBody.innerHTML = ''; // Leert den aktuellen Inhalt der Tabelle
+        const tableBody1 = document.getElementById('table-body1');
+        const tableBody2 = document.getElementById('table-body2');
+
+        tableBody1.innerHTML = ''; // Leert den aktuellen Inhalt der Tabelle
 
         // Begrenze die Anzahl der Einträge auf maximal 10
         data.slice(0, 5).forEach(fetch => {
@@ -24,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <td>${fetch[3]}</td>
                 <td>${fetch[4]}</td>
             `;
-            tableBody.appendChild(row);
+            tableBody1.appendChild(row);
         });
 
     }
@@ -35,6 +37,88 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initialisiere die Tabelle beim Laden der Seite
     fetchAndPopulateTable();
 });
+*/
+
+
+
+
+
+
+//////////////////
+
+var dataArr = [1, 2, 3, 4, 5];
+
+
+function arrbefuellen(){
+    
+    dataArr.push(Math.floor(Math.random() * (45 - 10 + 1)) + 10);
+
+    console.log("länge" + dataArr.length);
+    updateTable();
+}
+
+setInterval(arrbefuellen, 1000);
+
+
+
+var data;
+
+function updateTable() {
+    
+    console.log("Funktion start");
+    const tableBody1 = document.getElementById('table-body1');
+    const tableBody2 = document.getElementById('table-body2');
+    const row1 = document.createElement('tr');
+    
+
+    //row1.innerHTML = '';
+    //tableBody1 = '';
+
+
+    tableBody1.innerHTML = '';
+
+    if(dataArr.length > 5){
+
+        
+        for(var x = dataArr.length - 1; x > dataArr.length - 6 ; x--){
+
+            tableBody1.innerHTML += `
+            <th scope="row">test</th>
+            <td>test</td>
+            <td>${dataArr[x]}</td>
+            <td>test</td>
+            <td>test</td>
+        `;
+
+        }
+        
+    }else{
+
+        for(var i = 0; i < 5; i++){
+
+            tableBody1.innerHTML += `
+            <th scope="row">test</th>
+            <td>test</td>
+            <td>${dataArr[i]}</td>
+            <td>test</td>
+            <td>test</td>
+        `;
+
+        }
+
+
+    }
+
+
+
+
+
+    //tableBody1.appendChild(row1);
+
+}
+//////////////////////////////////////////////////////
+
+
 
 
 
@@ -45,9 +129,21 @@ function fanAnAus(t) {
   if (t > 30) {
     babell[0].style.backgroundColor = "green";
     babell[0].style.left = "3%";
+    babell[1].style.backgroundColor = "green";
+    babell[1].style.left = "3%";
   } else {
     babell[0].style.backgroundColor = "rgb(255, 28, 28)";
     babell[0].style.left = "calc(97% - 20px)";
+    babell[1].style.backgroundColor = "rgb(255, 28, 28)";
+    babell[1].style.left = "calc(97% - 20px)";
+  }
+
+  if (t > 30) {
+    babell[1].style.backgroundColor = "green";
+    babell[1].style.left = "3%";
+  } else {
+    babell[1].style.backgroundColor = "rgb(255, 28, 28)";
+    babell[1].style.left = "calc(97% - 20px)";
   }
 }
 
