09d15a0eb26f452dd259517133347434c1a04a16
Zerakles <jonas@grewe.eu>
2024-02-07 11:48:03 +0100

    FIXED - grafik.js - Table.js

--- web/js/Table.js
+++ web/js/Table.js
@@ -1,84 +1,61 @@
+var db1 = [];
+
 async function fetchData(pi) {
-  try {
-      let response;
-      if(pi === 1){
-          response = await fetch("http://172.20.199.251:8000/api/v1/temps?HID=Client_lVfU0rVUo&limit=5");
-      }else{
-          response = await fetch("http://172.20.199.251:8000/api/v1/temps?HID=Client_43PVPt8SW&limit=5");
-      }
-    if (!response.ok) {
-      console.log('Network response was not ok');
+    try {
+        let response;
+        if (pi === 1) {
+            response = await fetch("http://172.20.199.251:8000/api/v1/temps?HID=Client_TJlE8hDC7&limit=5");
+        }
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        db1 = data;
+        updateTable(db1, 1); // Update the first table data
+
+    } catch (error) {
+        console.error('Fetch error:', error);
     }
-    let db1 = await response.json();
-    db1 = JSON.parse(db1);
-    console.log(db1[0].id);
-  } catch (error) {
-    console.error('Fetch error:', error);
-  }
 }
-setInterval(updateTable, 1000);
-let data;
-function updateTable() {
-  let db1 = fetchData(1);
-  console.log("Funktion start");
-  let tableBody1 = document.getElementById('table-body1');
-  let tableBody2 = document.getElementById('table-body2');
-  tableBody1.innerHTML = '';
-  setTimeout(function () {
-      if(db1.length > 5){
-          for(let x = db1.length - 1; x > db1.length - 6 ; x--){
-              tableBody1.innerHTML += `
-              <th scope="row">${db1[x]['id']}</th>
-              <td>${db1[x].time}</td>
-              <td>${Math.round(db1[x]['temp_c'])}</td>
-              <td>${Math.round(db1[x]['temp_f'])}</td>
-              <td>Pi1</td>
-          `;
-          }
-      }else{
-          for(let i = 0; i < 5; i++){
-              tableBody1.innerHTML += `
-              <th scope="row">${db1[i].id}</th>
-              <td>${db1[i].time}</td>
-              <td>${Math.round(db1[i]['temp_c'])}</td>
-              <td>${Math.round(db1[i]['temp_f'])}</td>
-              <td>Pi2</td>
+
+fetchData(1);
+
+setInterval(fetchData, 2000, 1); // Fetch data from Pi1 every 2 seconds
+
+function updateTable(data, pi) {
+    console.log("Function start for Pi" + pi);
+    const tableBody = document.getElementById('table-body' + pi);
+    tableBody.innerHTML = '';
+    if (data.length > 0) {
+        for (var i = 0; i < Math.min(5, data.length); i++) {
+            tableBody.innerHTML += `
+                <tr>
+                    <th scope="row">${data[i].id}</th>
+                    <td>${data[i].time}</td>
+                    <td>${Math.round(data[i].temp_c)}</td>
+                    <td>${Math.round(data[i].temp_f)}</td>
+                    <td>Pi${pi}</td>
+                </tr>
             `;
-         }
-      }
-      fanAnAus(db1[0]['temp_c'], 1);
-      let db2 = fetchData(2);
-      if(db2.length > 5){
-          for(let x = db2.length - 1; x > db2.length - 6 ; x--){
-              tableBody2.innerHTML += `
-              <th scope="row">${db2[x]['id']}</th>
-              <td>${db2[x].time}</td>
-              <td>${Math.round(db2[x]['temp_c'])}</td>
-              <td>${Math.round(db2[x]['temp_f'])}</td>
-              <td>Pi1</td>
-          `;
-          }
-      }else{
-          for(let i = 0; i < 5; i++){
-              tableBody2.innerHTML += `
-              <th scope="row">${db2[i]['id']}</th>
-              <td>${db1[i].time}</td>
-              <td>${Math.round(db2[i]['temp_c'])}</td>
-              <td>${Math.round(db2[i]['temp_f'])}</td>
-              <td>Pi2</td>
-          `;
-          }
-      }
-      fanAnAus(db1[0]['temp_c'], 2);
-  }, 200);
+        }
+    }
 }
-const babell = document.getElementsByClassName("babell");
+
 function fanAnAus(t, pi) {
-    if (t > 28) {
-        babell[pi].style.backgroundColor = "green";
-        babell[pi].style.left = "3%";
+    const babell = document.getElementsByClassName("babell")[pi - 1];
+    if (t > 30) {
+        babell.style.backgroundColor = "green";
+        babell.style.left = "3%";
     } else {
-        babell[pi].style.backgroundColor = "rgb(255, 28, 28)";
-        babell[pi].style.left = "calc(97% - 20px)";
+        babell.style.backgroundColor = "rgb(255, 28, 28)";
+        babell.style.left = "calc(97% - 20px)";
+    }
+}
+
+function addT(pi) {
+    if (db1.length > 0) {
+        fanAnAus(db1[0].temp_c, pi);
     }
 }
--- web/js/grafik.js
+++ web/js/grafik.js
@@ -6,48 +6,39 @@ var tempD2 = ["09:01","09:02","09:03","09:04","09:05","09:06","09:07","09:08","0
 var db1;
 
 async function fetchData(p) {
-   let response;
-   try {
+    let response;
+    try {
 
-      if(p === 1){
-          response = await fetch("http://172.20.199.251:8000/api/v1/temps?HID=Client_3PNikenZs&limit=1");
-      }else{
-          response = await fetch("http://172.20.199.251:8000/api/v1/temps?HID=Client_43PVPt8SW&limit=1");
-      }
-     
- 
-     if (!response.ok) {
-       throw new Error('Network response was not ok');
-     }
- 
- 
- 
-     const data = await response.json();
-     // Hier kannst du mit den geladenen Daten arbeiten
-     db1 = data;
-     db1 = JSON.parse(db1);
-   } catch (error) {
-     console.error('Fetch error:', error);
-   }
- }
- 
- // Aufruf der Funktion
- fetchData(1);
- fetchData(2);
- 
- 
- 
- 
- 
- setTimeout(function () {
-   
-   
- },1200);
+        if(p === 1){
+            response = await fetch("http://172.20.199.251:8000/api/v1/temps?HID=Client_TJlE8hDC7&limit=1");
+        }else{
+            response = await fetch("http://172.20.199.251:8000/api/v1/temps?HID=Client_TJlE8hDC7&limit=1");
+        }
+
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+
+
+        const data = await response.json();
+        // Hier kannst du mit den geladenen Daten arbeiten
+        db1 = data;
+        db1 = JSON.parse(db1);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+}
+
+// Aufruf der Funktion
+fetchData(1);
+fetchData(2);
+
 
 
 
 var container = document.getElementsByClassName("Container");
-var container2 = document.getElementsByClassName("Container2");
 var color = "rgb(0, 179, 255)";
 var Prozent;
 var intF = true;
@@ -55,84 +46,56 @@ var intF = true;
 
 
 function temptoProzent(t){
-   Prozent = Math.round(t / 1.2);
-   return Prozent;
+    Prozent = Math.round(t / 1.2);
+    return Prozent;
 }
 
 
 function q_einfügen(c){
     var tPark;
-         x=0;
-   if(c == 0){
-      if(temp.length > 10 ){
-         container[0].innerHTML = `<span class="T15">15° -</span>
+    x=0;
+    if(c == 0){
+        if(temp.length > 10 ){
+            container[0].innerHTML = `<span class="T15">15° -</span>
          <span class="T30">30° -</span>
          <span class="T60">60° -</span>
          <span class="T90">90° -</span>
          <span class="T120">120° -</span>`;
-         for(y = temp.length - 10; y < temp.length; y++){
-         color = tempgrad(temp[y]);
-         tPark = temptoProzent(temp[y]);
+            for(y = temp.length - 10; y < temp.length; y++){
+                color = tempgrad(temp[y]);
+                tPark = temptoProzent(temp[y]);
 
-         container[0].innerHTML += `<div class="q" style="height:${tPark}%; background-color: ${color};"><span class="temp">${temp[y]}°</span> <span class="tempD">${tempD[y]}</span></div>`;
-         x++
-         }
-      }else{
-         for(i = 0; i < temp.length; i++){
-         color = tempgrad(temp[i]);
-         tPark = temptoProzent(temp[i])
-          container[0].innerHTML += `<div class="q" style="height:${tPark}%; background-color: ${color};"><span class="temp">${temp[i]}°</span> <span class="tempD">${tempD[y]}</span></div>`;
-          x++
-
-
-         }
-      }
-
-   }else{
-      if(temp2.length > 10 ){
-         container2[0].innerHTML = `<span class="T15">15° -</span>
-         <span class="T30">30° -</span>
-         <span class="T60">60° -</span>
-         <span class="T90">90° -</span>
-         <span class="T120">120° -</span>`;
-         for(y = temp2.length - 10; y < temp2.length; y++){
-         color = tempgrad(temp2[y]);
-         tPark = temptoProzent(temp2[y]);
-
-         container2[0].innerHTML += `<div class="q" style="height:${tPark}%; background-color: ${color};"><span class="temp">${temp2[y]}°</span> <span class="tempD">${tempD2[y]}</span></div>`;
-         x++
-         }
-      }else{
-         for(i = 0; i < temp2.length; i++){
-         color = tempgrad(temp2[i]);
-         tPark = temptoProzent(temp2[i])
-          container2[0].innerHTML += `<div class="q" style="height:${tPark}%; background-color: ${color};"><span class="temp">${temp2[i]}°</span> <span class="tempD">${tempD2[y]}</span></div>`;
-          x++
+                container[0].innerHTML += `<div class="q" style="height:${tPark}%; background-color: ${color};"><span class="temp">${temp[y]}°</span> <span class="tempD">${tempD[y]}</span></div>`;
+                x++
+            }
+        }else{
+            for(i = 0; i < temp.length; i++){
+                color = tempgrad(temp[i]);
+                tPark = temptoProzent(temp[i])
+                container[0].innerHTML += `<div class="q" style="height:${tPark}%; background-color: ${color};"><span class="temp">${temp[i]}°</span> <span class="tempD">${tempD[y]}</span></div>`;
+                x++
 
 
-         }
-      }
-   }
-        
+            }
+        }
 
-
-
+    }
 }
 
 function tempgrad(t){
-   if(t >= 0 && t < 25 ){
-    color = "rgb(0, 179, 255)";
-   }else if(t >= 25 && t < 30){
-    color = "rgb(255, 204, 0)";
-   }else if(t >= 30 && t < 45){
-    color = "orange";
-   }else if(t >= 45 && t < 60){
-    color = "rgb(255, 77, 0)";
-   }else{
-    color = "rgb(255, 28, 28)";
-   }
+    if(t >= 0 && t < 25 ){
+        color = "rgb(0, 179, 255)";
+    }else if(t >= 25 && t < 30){
+        color = "rgb(255, 204, 0)";
+    }else if(t >= 30 && t < 45){
+        color = "orange";
+    }else if(t >= 45 && t < 60){
+        color = "rgb(255, 77, 0)";
+    }else{
+        color = "rgb(255, 28, 28)";
+    }
 
-   return color;
+    return color;
 }
 
 
@@ -143,81 +106,52 @@ var park;
 var parkt;
 function addT(){
 
-   fetchData(1)
+    fetchData(1)
     setTimeout(function () {
         park = Math.round(db1[0].temp_c);
         parkt = db1[0].time;
 
         fanAnAus(park,1);
-         temp.push(park);
-         tempD.push(db1[0].time.substring(11))
-     
-         
-         q_einfügen(1)
-   
-    },  200);
-}
-var park2;
-var parkt2;
-function addT2(){
+        temp.push(park);
+        tempD.push(db1[0].time.substring(11))
 
-   fetchData(2)
-    setTimeout(function () {
-        park2 = Math.round(db1[0].temp_c);
-        parkt2 = db1[0].time;
 
-       
-        fanAnAus(park2,2);
-         temp2.push(park2);
-         tempD2.push(db1[0].time.substring(11))
-     
-       
-         q_einfügen(1)
-   
-    },  200);
+        q_einfügen(0)
+
+    },  2000);
 }
+
 
 var setaddT = setInterval(addT, 1000);
-var setaddT = setInterval(addT2, 1000);
+
 
 function anundaus(){
-   if(intF === true){
-          clearInterval(setaddT);
-          intF = false;
-   }else{
-      setaddT = setInterval(addT, 1000);
-          intF = true;
-   }
+    if(intF === true){
+        clearInterval(setaddT);
+        intF = false;
+    }else{
+        setaddT = setInterval(addT, 5000);
+        intF = true;
+    }
 }
 
 
 
 
 var babell = document.getElementsByClassName("babell");
-var babell2 = document.getElementsByClassName("babell2");
 
 function fanAnAus(t,f){
-   if(f == 1){
-      if(t > 30){
-         babell[0].style.backgroundColor = "green"
-         babell[0].style.left = "3%";
-         
-         }else{
+    if(f == 1){
+        if(t > 30){
+            babell[0].style.backgroundColor = "green"
+            babell[0].style.left = "3%";
+
+        }else{
             babell[0].style.backgroundColor = "rgb(255, 28, 28)"
             babell[0].style.left = "calc(97% - 20px)";
-         }
+        }
 
-   }else{
-      if(t > 30){
-         babell2[0].style.backgroundColor = "green"
-         babell2[0].style.left = "3%";
-         
-         }else{
-            babell2[0].style.backgroundColor = "rgb(255, 28, 28)"
-            babell2[0].style.left = "calc(97% - 20px)";
-         }
-
-   }
+    }
 
 }
 
@@ -225,15 +159,15 @@ var testa = [28,30,30,30,32]
 
 
 function checkAvgTemp(t){
-   var sum = 0;
-   for(var i =0 ; i < t.length; i++ ){
-      sum += t[i];
-   }
-   if (sum / 5 >= 30){
-      return true;
-   }else{
-      return false;
-   }
+    var sum = 0;
+    for(var i =0 ; i < t.length; i++ ){
+        sum += t[i];
+    }
+    if (sum / 5 >= 30){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 console.log(checkAvgTemp(testa));
